cd43a06023dba0eb753669ab54dc19bbb450ad40
전민오 <jbj338033@gmail.com>
2024-07-14 11:43:14 +0900

    First Commit

--- src/main/kotlin/com/open3r/openchat/OpenChatApplication.kt
+++ src/main/kotlin/com/open3r/openchat/OpenChatApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class OpenchatApplication
+class OpenChatApplication
 
 fun main(args: Array<String>) {
-    runApplication<OpenchatApplication>(*args)
+    runApplication<OpenChatApplication>(*args)
 }
